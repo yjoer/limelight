291778e97461df72608e56bddc0ce0eb19f190e8
Yeoh Joer <yeoh.joer@outlook.com>
2020-04-27 00:20:41 +0800

    Execute content scripts after the page has loaded

--- extension/inject.js
+++ extension/inject.js
@@ -1,12 +1,16 @@
-console.log("Start");
-var t0 = performance.now();
+const inject = () => {
+  console.log("Start");
+  var t0 = performance.now();
 
-var s = document.createElement("script");
-s.src = chrome.runtime.getURL("remove-event-listeners.js");
-s.onload = function () {
-  this.remove();
-  var t1 = performance.now();
-  console.log(`End: ${t1 - t0} milliseconds`);
+  var s = document.createElement("script");
+  s.src = chrome.runtime.getURL("remove-event-listeners.js");
+  s.onload = function () {
+    this.remove();
+    var t1 = performance.now();
+    console.log(`End: ${t1 - t0} milliseconds`);
+  };
+
+  (document.head || document.documentElement).appendChild(s);
 };
 
-(document.head || document.documentElement).appendChild(s);
+window.addEventListener("load", inject);
