85a69f20b3ab838eb9324e0c6cc5355540def778
Yeoh Joer <yeoh.joer@outlook.com>
2020-04-29 14:54:30 +0800

    Capture keyboard events of cut, copy and paste precisely

--- extension/remove-event-listeners.js
+++ extension/remove-event-listeners.js
@@ -1,10 +1,5 @@
 // https://github.com/moodle/moodle/blob/master/mod/quiz/module.js#L203
 
-// Rough
-// Y.one('body').detach()
-// Y.one('document').detach()
-// Y.all('*').detach()
-
 let documentEvents = [
   "contextmenu",
   "dragstart",
@@ -13,27 +8,102 @@ let documentEvents = [
   "copy",
   "paste",
 ];
-let bodyEvents = ["mousedown", "mouseup"];
+let mouseEvents = ["mousedown", "mouseup"];
 let keyboardEvents = ["keypress", "keyup", "keydown"];
 
+const CTRL_KEY = 17;
+const COMMAND_KEY = 91;
+const C_KEY = 67;
+const V_KEY = 86;
+const X_KEY = 88;
+
+let keyState = {
+  ctrlKeyDown: false,
+};
+
+const interceptMouseEvents = (e) => {
+  let classes = ["ace_content"];
+
+  if (mouseEvents.includes(e.type)) {
+    for (className of classes) {
+      if (e.target.classList.contains(className)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const interceptKeyboardEvents = (e) => {
+  if (e.type == "keydown") {
+    if (e.keyCode == CTRL_KEY || e.keyCode == COMMAND_KEY) {
+      keyState.ctrlKeyDown = true;
+    }
+  }
+
+  if (e.type == "keyup") {
+    if (e.keyCode == CTRL_KEY || e.keyCode == COMMAND_KEY) {
+      keyState.ctrlKeyDown = false;
+    }
+  }
+
+  if (e.type == "keyup" || e.type == "keydown") {
+    console.log(e.target);
+    if (keyState.ctrlKeyDown && [C_KEY, V_KEY, X_KEY].includes(e.keyCode)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 // Event capturing
-let events = [...documentEvents, ...bodyEvents, ...keyboardEvents];
-events.forEach((event) => {
+for (documentEvent of documentEvents) {
   window.addEventListener(
-    event,
-    (e) => {
-      e.stopPropagation();
+    documentEvent,
+    (event) => {
+      event.stopPropagation();
     },
     true
   );
-});
+}
+
+for (mouseEvent of mouseEvents) {
+  window.addEventListener(
+    mouseEvent,
+    (event) => {
+      if (interceptMouseEvents(event)) {
+        event.stopPropagation();
+      }
+    },
+    true
+  );
+}
+
+for (keyboardEvent of keyboardEvents) {
+  window.addEventListener(
+    keyboardEvent,
+    (event) => {
+      if (interceptKeyboardEvents(event)) {
+        event.stopPropagation();
+      }
+    },
+    true
+  );
+}
+
+// Rough
+// Y.one('body').detach()
+// Y.one('document').detach()
+// Y.all('*').detach()
 
 // Precise
 // documentEvents.forEach((e) => {
 //   Y.one("document").detach(e);
 // });
 
-// bodyEvents.forEach((e) => {
+// mouseEvents.forEach((e) => {
 //   Y.one("body").detach(e);
 // });
 
