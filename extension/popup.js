a9cb6259651cdb6cede35d49b84c3be85dadfb8e
Yeoh Joer <yeoh.joer@outlook.com>
2020-10-20 18:30:24 +0800

    Display hostname of the active tab being injected with scripts

---
+++ extension/popup.js
@@ -0,0 +1,7 @@
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  let currentTab = tabs[0];
+  let url = new URL(currentTab.url);
+
+  let elements = document.getElementsByClassName("settings__item-text");
+  elements[0].innerHTML += `<b>${url.hostname}</b>`;
+});
