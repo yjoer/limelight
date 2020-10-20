740b0949e69db038204ff7680fe1205b3c348fe9
Yeoh Joer <yeoh.joer@outlook.com>
2020-10-20 12:49:33 +0800

    Add user interface to select scripts for injection

--- extension/popup.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById("toggle");
-
-button.addEventListener("click", () => {
-  if (button.innerText == "Off") {
-    button.innerText = "On";
-  } else {
-    button.innerText = "Off";
-  }
-});
