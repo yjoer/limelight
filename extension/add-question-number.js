d26419e9fd730f978e851747cadf513aab7b859c
Yeoh Joer <yeoh.joer@outlook.com>
2022-01-10 15:24:11 +0800

    Show question numbers hidden in element identifiers

---
+++ extension/add-question-number.js
@@ -0,0 +1,31 @@
+const buttonList = document.querySelector("#mod_quiz_navblock .qn_buttons");
+
+for (const child of buttonList.children) {
+  if (!child.id) {
+    continue;
+  }
+
+  const match = child.id.match(/^([a-z]+)(\d+)$/);
+
+  if (match.length !== 3) {
+    continue;
+  }
+
+  const questionNumber = match[2];
+
+  const newNode = document.createElement("div");
+  newNode.innerHTML = `(${questionNumber})`;
+  newNode.style.color = "hsla(0, 0%, 0%, 0.56)";
+
+  for (const childNode of child.childNodes) {
+    if (childNode.nodeType !== Node.TEXT_NODE) {
+      continue;
+    }
+
+    child.insertBefore(newNode, childNode.nextSibling);
+  }
+
+  child.style.setProperty("height", "auto", "important");
+  child.style.setProperty("line-height", "1rem", "important");
+  child.style.setProperty("padding", "4px 0", "important");
+}
